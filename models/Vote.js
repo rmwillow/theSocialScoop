74ce0f42aa082864a414fb4c6988547cbdf7c3ec
Meghan Bock <meghan.bock@gmail.com>
2021-08-12 15:00:38 -0500

    setup vote model

---
+++ models/Vote.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Vote extends Model {}
+
+Vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    review_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'review',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = Vote;
