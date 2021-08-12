d74d61e2b528a6ae98a6d75d63873f415b9c8364
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-12 18:14:50 -0500

    create show model

---
+++ models/Show.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// create Show model
+class Show extends Model {}
+
+// create fields/columns for Show model
+Show.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overview: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    poster_path: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.ARRAY,
+      allowNull: false,
+    },
+    season_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    episode_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    // add rating value HERE once rating model has been built
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "show",
+  }
+);
+
+module.exports = Show;
---
+++ seeds/show-seeds.js
@@ -0,0 +1,22 @@
+const { Show } = require('../models');
+
+const showData = [
+    {
+        title: "Loki",
+        overview: "After stealing the Tesseract during the events of 'Avengers: Endgame,' an alternate version of Loki is brought to the mysterious Time Variance Authority, a bureaucratic organization that exists outside of time and space and monitors the timeline. They give Loki a choice: face being erased from existence due to being a 'time variant' or help fix the timeline and stop a greater threat.",
+        poster_path: "/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+        genre: ["Drama", "Sci-Fi & Fantasy"],
+        season_count: 1,
+        episode_count: 6,
+        apiId: 84958
+    },
+    {
+        title: "Jeopardy!",
+        overview: "America's favorite quiz show where contestants are presented with general knowledge clues in the form of answers, and must phrase their responses in question form.",
+        poster_path: "/hnKAKk7T9MvWaLoOymVNAzaGZf8.jpg",
+        genre: ["Game Show"],
+        season_count: 38,
+        episode_count: 7132,
+        apiId: 2912
+    },
+]
