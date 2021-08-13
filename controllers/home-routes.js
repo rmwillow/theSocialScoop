e5ef194cd49deed46cb6c86bf03d2dbfe9c699be
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-13 15:09:52 -0400

    get all reviews for home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,4 +2,20 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Rating, Show, Review, Vote } = require('../models');
 
+// get all reviews for homepage
+router.get("/", (req, res) => {
+    Review.findAll({
+      include: [User],
+    })
+      .then((dbReviewData) => {
+        const reviews = dbReviewData.map((review) => review.get({ plain: true }));
+  
+        res.render("all-reviews", { reviews });
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+
+
 module.exports = router;
