770027931b097b90f3d87b6618908b9551b31564
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-12 21:18:49 -0400

    chnaging seeds data

--- seeds/rating-seeds.js
+++ seeds/rating-seeds.js
@@ -1,8 +1,18 @@
 const ratingData = [
     {
-        user_id: 9,
-        show_id: 19,
+        user_id: 6,
+        show_id: 12,
         rating: 1,
+    },
+    {
+        user_id: 9,
+        show_id: 24,
+        rating: 2,
+    },
+    {
+        user_id: 7,
+        show_id: 45,
+        rating: 3,
     }
 ]
 
