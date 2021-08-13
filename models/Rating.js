491bafcb70204cfede9e2a84af818122b85abf96
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-12 20:51:32 -0400

    added rating class data

--- models/Rating.js
+++ models/Rating.js
@@ -1,9 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Rating extends Model {
+class Rating extends Model {}
 
+Rating.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      show_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'show',
+          key: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'rating'
+    }
+  );
 
-}
 
 module.exports = Rating;
