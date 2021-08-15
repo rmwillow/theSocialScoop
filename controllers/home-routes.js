9b1eb345510fe8f96ebaec747b3aee61eb51e72d
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-15 17:18:11 -0500

    create handlebars template and css for main, add temp homepage and login page routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,3 +3,11 @@ const sequelize = require('../config/connection');
 const { User, Rating, Show, Review, Vote } = require('../models');
 
 module.exports = router;
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes');
+const homeRoutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
