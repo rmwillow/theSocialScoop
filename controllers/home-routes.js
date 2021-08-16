605b44007a314824ae23648715ca5a43ed5621e6
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-16 15:12:07 -0500

    add poster links to homepage, create route for homepage to get show info

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,83 @@
-const router = require('express').Router();
-const { User, Rating, Show, Review, Vote } = require('../models');
+const router = require("express").Router();
+const { User, Rating, Show, Review, Vote } = require("../models");
+const sequelize = require('../config/connection');
 
 // get all reviews for homepage
 router.get("/", (req, res) => {
-  res.render('homepage');
+  Show.findAll({
+    attributes: [
+      "id",
+      "title",
+      "overview",
+      "poster_path",
+      "genre",
+      "season_count",
+      "episode_count",
+      [
+        sequelize.literal(
+          "(SELECT AVG(rating) FROM rating WHERE show.id = rating.show_id)"
+        ),
+        "rating_average",
+      ],
+    ],
+    include: [
+      {
+        model: Review,
+        attributes: [
+          "id",
+          "review_text",
+          "user_id",
+          "show_id",
+          "date_watched",
+          "created_at",
+        ],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((showData) => {
+      const shows = showData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { shows });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-  //   Review.findAll({
-  //     include: [User],
-  //   })
-  //     .then((dbReviewData) => {
-  //       const reviews = dbReviewData.map((review) => review.get({ plain: true }));
-  
-  //       res.render("all-reviews", { reviews });
-  //     })
-  //     .catch((err) => {
-  //       res.status(500).json(err);
-  //     });
-  });
+//   Review.findAll({
+//     include: [User],
+//   })
+//     .then((dbReviewData) => {
+//       const reviews = dbReviewData.map((review) => review.get({ plain: true }));
 
-  // Login, Logout 7 Signup routes
-  router.get("/login", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-  
-    res.render("login");
-  });
-  
-  router.get("/signup", (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect("/");
-      return;
-    }
-  
-    res.render("signup");
-  });
+//       res.render("all-reviews", { reviews });
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     });
+    
+
+// Login, Logout 7 Signup routes
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
 
 module.exports = router;
