cd732b4fcdc4767b29d523c9be39f9d1453a04a8
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-16 18:02:28 -0500

    changed server.js

--- server.js
+++ server.js
@@ -24,21 +24,31 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({
+  helpers: {
+    format_date: date => {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
+  }
+});
 
 // set up handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//setting the middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
-app.use(routes);
+// app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening on PORT 3001'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
