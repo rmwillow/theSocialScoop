f579dd5dabfe5c9403889f163dd9e12452c86246
Daniel O <danielolvera2188@gmail.com>
2021-08-17 18:29:04 -0600

    star rating

--- public/javascript/rate-show.js
+++ public/javascript/rate-show.js
@@ -1,28 +1,29 @@
+var starRatingControl = new StarRating('.star-rating');
+
 async function rateShowClickHandler(event) {
-    event.preventDefault();
-  
-    const rating = document
-    .querySelector('X[name="rating-value"]').value.trim(); // need to update X with correct query selector
-    const show_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch('/api/shows/rate', {
-      method: 'PUT',
-      body: JSON.stringify({
-        show_id,
-        rating
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
+  event.preventDefault();
+
+  const rating = document
+    .querySelector('starRatingControl[name=.star-rating"]').value.trim(); // need to update X with correct query selector
+  const show_id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch('/api/shows/rate', {
+    method: 'PUT',
+    body: JSON.stringify({
+      show_id,
+      rating
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.star-rating').addEventListener('click', rateShowClickHandler);
-  
+}
+
+document.querySelector('.star-rating').addEventListener('click', rateShowClickHandler);
