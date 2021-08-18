7d0edec2e333692a85b0ea1f1a8272382d5462ff
Daniel O <danielolvera2188@gmail.com>
2021-08-17 19:25:43 -0600

    committing to pull from develop

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,7 +1,5 @@
-const { response } = require("express");
-
 async function logout() {
-    const respose = await fetch('/api/users/logout', {
+    const response = await fetch('/api/users/logout', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' }
     });
