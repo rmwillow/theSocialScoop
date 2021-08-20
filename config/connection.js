45509645b5cbcd44efefad95af563ce68334e6ab
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-19 21:06:41 -0500

    works

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,33 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-// require('dotenv').config();
+// // require('dotenv').config();
 
-// create connection to db, whether local on on heroku using jawsdb
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+// // create connection to db, whether local on on heroku using jawsdb
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   });
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+var mysql = require("mysql");
+var connection;
+//make connection
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "Lemon123",
+    database: "broadcastr_db"
+  })
+}
+connection.connect();
+// Export connection for our ORM to use.
+module.exports = connection;
 
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const routes = require('./controllers');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
