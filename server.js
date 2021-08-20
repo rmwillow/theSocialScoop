e0ca5262490b9f8dd653ad4f3140fe7193938dbb
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-20 15:49:07 -0500

    changed port

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
