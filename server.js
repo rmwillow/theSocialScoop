5456a72cbe0cb57bca0eab6a6d71e434d1495a26
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-20 12:59:54 -0500

    testing work

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const handlebars = require("handlebars");
-const routes = require("./controllers");
 
 const app = express();
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //cookies
 const sess = {
@@ -23,51 +21,19 @@ const sess = {
 };
 
 
+
 app.use(session(sess));
 
-const hbs = exphbs.create({
-  helpers:
-    {
-      format_date: (date) => {
-        return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-      },
-      ifEquals: (arg1, arg2, options) => {
-        return arg1 == arg2 ? options.fn(this) : options.inverse(this);
-      },
-      ifNotEquals: (arg1, arg2, options) => {
-        return arg1 !== arg2 ? options.fn(this) : options.inverse(this);
-      },
-      ifNullEmptyZero: (arg1, options) => {
-        return !arg1 || arg1 == 0 ? options.fn(this) : options.inverse(this);
-      },
-      ifGreaterThan: (arg1, arg2, options) => {
-        return arg1 > arg2 ? options.fn(this) : options.inverse(this);
-      },
-      selectRating: (arg1) => {
-        console.log("arg1: " + arg1);
-        if (arg1 == 1) {
-          return new handlebars.SafeString("<option value=''></option><option value='5'></option><option value='4'></option><option value='3'></option><option value='2'></option><option value='1'selected></option>")
-        } else if(arg1 == 2) {
-          return new handlebars.SafeString("<option value=''></option><option value='5'></option><option value='4'></option><option value='3'></option><option value='2'selected></option><option value='1'></option>")
-        } else if(arg1 == 3) {
-          return new handlebars.SafeString("<option value=''></option><option value='5'></option><option value='4'></option><option value='3' selected></option><option value='2'></option><option value='1'></option>")
-        } else if (arg1 == 4) {
-          return new handlebars.SafeString("<option value=''></option><option value='5'></option><option value='4' selected></option><option value='3'></option><option value='2'></option><option value='1'></option>")
-        } else if (arg1 == 5) {
-          return new handlebars.SafeString("<option value=''></option><option value='5' selected></option><option value='4'></option><option value='3'></option><option value='2'></option><option value='1'></option>")
-        }
-        return new handlebars.SafeString("<option value='' selected></option><option value='5'></option><option value='4'></option><option value='3'></option><option value='2'></option><option value='1'></option>")
-      }
-    }
-});
+const helpers = require('./utils/helpers');
 
+const hbs = exphbs.create({ helpers });
 // set up handlebars
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 // express middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 //setting the middleware
 app.use(express.static(path.join(__dirname, "public")));
@@ -76,16 +42,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // app.use(routes);
 app.use(require("./controllers/"));
 
-// turn on connection to db and server
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${ PORT }`);
-//   sequelize.sync({ force: false });
-// });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
-
-exports = module.exports = app;
