47e8fc3d62696a03abe6a03648d5340461f35606
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-20 11:11:04 -0500

    testing

---
+++ public/javascript/knexfile.js
@@ -0,0 +1,14 @@
+module.exports = { 
+    devlopment: {
+        client: 'pg',
+        connection: 'postgres://localhost/cjs-web-store'
+    },
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost/cjs-web-store'
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL
+    }
+}
--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.use(require("./controllers/"));
 // });
 
 let port = process.env.PORT;
-if (port == null || port == "") { port = 3000;};
-app.listen(port, function () { console.log("Server started successfully");});
-
-
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
 
 exports = module.exports = app;
