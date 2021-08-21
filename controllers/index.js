a8cd25b8f2ab3e75bf95ea869ad19eb1157033e4
rmwillow <52016382+rmwillow@users.noreply.github.com>
2021-08-21 18:15:26 -0500

    testing error fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./user-dashboard-routes.js');
@@ -6,5 +7,6 @@ const dashboardRoutes = require('./user-dashboard-routes.js');
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
+
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,33 +1,5 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-const handlebars = require("handlebars");
-const routes = require("./controllers");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-
-//cookies
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
-
-const hbs = exphbs.create({
-  helpers:
-    {
-      format_date: (date) => {
+module.exports = {
+    format_date: date => {
         return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
       },
       ifEquals: (arg1, arg2, options) => {
@@ -57,26 +29,4 @@ const hbs = exphbs.create({
         }
         return new handlebars.SafeString("<option value='' selected></option><option value='5'></option><option value='4'></option><option value='3'></option><option value='2'></option><option value='1'></option>")
       }
-    }
-});
-
-// set up handlebars
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-// express middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-//setting the middleware
-app.use(express.static(path.join(__dirname, "public")));
-
-// turn on routes
-// app.use(routes);
-app.use(require("./controllers/"));
-
-// turn on connection to db and server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
+}
